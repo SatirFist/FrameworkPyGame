fb4c24ca7c14cda9861f2a1b45e85441c09fafc1
Erik Johnson <satirfist@gmail.com>
2023-05-04 15:47:30 -0700

    Audio cleanup

--- audio_class.py
+++ audio_class.py
@@ -9,23 +9,16 @@ class App_SoundsClass():
     Clip2 = None    
 
     def __init__(self):
-        mixer.init()
-                
-        #self.MenuSong = mixer.Sound("./Audio/Menu.wav")                    
-        #self.GameSong = mixer.Sound("./Audio/Game.wav")                
+        mixer.init()              
         self.Clip1 = mixer.Sound("./Audio/Right.wav")
         self.Clip2 = mixer.Sound("./Audio/Wrong.wav")
 
     def handle_event(self, event):
         if event.type == SWITCH_TO_MENU:
-            #self.GameSong.stop()            
-            #self.MenuSong.play()
             pygame.mixer.music.pause()
             pygame.mixer.music.load("./Audio/Menu.wav")
             pygame.mixer.music.play(-1) # Play forever
-        if event.type == START_COUNTDOWN_TO_GAME:
-            #self.MenuSong.stop()
-            #self.GameSong.play()    
+        if event.type == START_COUNTDOWN_TO_GAME:  
             pygame.mixer.music.pause()
             pygame.mixer.music.load("./Audio/Game.wav")
             pygame.mixer.music.play(-1) # Play forever                        
@@ -33,12 +26,3 @@ class App_SoundsClass():
             self.Clip1.play()
         if event.type == PLAY_CLIP2:
             self.Clip2.play()
-
-
-            '''
-            pygame.init()
-
-pygame.mixer.init()
-pygame.mixer.music.load('music_01.mp3')
-pygame.mixer.music.play(-1)
-'''
